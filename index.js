b79128db3c8a7e58a3445eb82a5a3f1b2013d039
+ <joshngaara@gmail.com>
2025-01-08 22:07:24 +0200

    lookup  js alert sh

---
+++ index.js
@@ -0,0 +1,13 @@
+var selectedRow = null;
+
+//show user alertrs
+function showAlert(message, className){
+    const dive = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    
+
+
+}
